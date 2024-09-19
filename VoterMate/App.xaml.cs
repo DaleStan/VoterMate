8284230a123be5fe6f4406354ccd6d8f33618e8e
Dale McCoy <21223975+DaleStan@users.noreply.github.com>
2024-09-19 10:48:45 -0400

    Maui template solution

---
+++ VoterMate/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace VoterMate
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ VoterMate/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace VoterMate
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
