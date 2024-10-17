DaleStan/VoterMate

commit ced2347cba49cd42bd4aec5328fdd80b45ba75db

Spray async/await around to improve startup time.

VoterMate/LookupPage.xaml.cs
@@ -4,6 +4,13 @@ namespace VoterMate;
 
 public partial class LookupPage : ContentPage
 {
+    private static readonly Task<List<string>> _nameParts;
+
+    static LookupPage()
+    {
+        _nameParts = Task.Run(static async () => (await App.Database.GetNamePartsAsync()).ToList());
+    }
+
     private readonly MainPage _mainPage;
 
     public LookupPage(MainPage mainPage)
@@ -12,28 +19,32 @@ public partial class LookupPage : ContentPage
         _mainPage = mainPage;
     }
 
-    private void ContentPage_Loaded(object sender, EventArgs e)
+    private async void ContentPage_Loaded(object sender, EventArgs e)
     {
-        acVoterName.ItemsSource = App.Database.GetNameParts();
-        acVoterName.Focus();
+        if (!_nameParts.Wait(100))
+            await _nameParts;
+
+        acVoterName.ItemsSource = _nameParts.Result;
+        acVoterName.IsEnabled = true;
+        acVoterName.Placeholder = "Enter name, age, and/or address";
     }
 
     private async void Lookup_Clicked(object sender, EventArgs e)
     {
-        var info = App.Database.GetMobilizer("OH" + txtVoterID.Text);
+        var info = App.Database.GetMobilizerAsync("OH" + txtVoterID.Text);
         if (info == null)
         {
             await DisplayAlert("Not Found", $"The voter with ID OH{txtVoterID.Text} could not be found.", "OK");
             return;
         }
 
-        var (mobilizer, location) = info.Value;
+        var (mobilizer, location) = (await info).Value;
         _mainPage.LogEvent("Opening mobilizer page (ID lookup)", mobilizer.ID, _mainPage.Location);
         await txtVoterID.HideSoftInputAsync(new CancellationTokenSource().Token);
-        await Navigation.PushAsync(new MobilizerPage(location, mobilizer, _mainPage));
+        await Navigation.PushAsync(new MobilizerPage(location, mobilizer, await App.Database.GetPriorityVotersAsync(location, mobilizer), _mainPage));
     }
 
-    private void acVoterName_SelectionChanged(object sender, Syncfusion.Maui.Inputs.SelectionChangedEventArgs e)
+    private async void acVoterName_SelectionChanged(object sender, Syncfusion.Maui.Inputs.SelectionChangedEventArgs e)
     {
         if (acVoterName.SelectedItems?.Count > 0 && txtVoterName.Text != "")
         {
@@ -41,10 +52,13 @@ public partial class LookupPage : ContentPage
         }
 
         lblWarning.IsVisible = false;
-        var lists = acVoterName.SelectedItems?.Cast<string>().Select(App.Database.GetVoters).ToList() ?? [];
+        var taskLists = acVoterName.SelectedItems?.Cast<string>().Select(App.Database.GetVotersAsync).ToList() ?? [];
+        await Task.WhenAll(taskLists);
+        var lists = taskLists.Select(t => t.Result).ToList();
 
         if (lists.Count == 0)
         {
+            ConfigureVoterSelection([]);
             cboVoterName.Text = "No search parameters";
         }
         else
@@ -73,10 +87,10 @@ public partial class LookupPage : ContentPage
             } while (!txtVoterName.IsFocused);
         }
 
-        ConfigureVoterSelection(App.Database.GetVotersByName(txtVoterName.Text));
+        ConfigureVoterSelection(await App.Database.GetVotersByNameAsync(txtVoterName.Text));
     }
 
-    private void DatePicker_DateSelected(object sender, DateChangedEventArgs e)
+    private async void DatePicker_DateSelected(object sender, DateChangedEventArgs e)
     {
         if (acVoterName.SelectedItems?.Count > 0 || txtVoterName.Text != "")
         {
@@ -84,7 +98,7 @@ public partial class LookupPage : ContentPage
             acVoterName.SelectedItems?.Clear();
         }
 
-        ConfigureVoterSelection(App.Database.GetVotersByBirthdate(e.NewDate));
+        ConfigureVoterSelection(await App.Database.GetVotersByBirthdateAsync(e.NewDate));
     }
 
 

VoterMate.Database/IDatabase.cs
@@ -5,16 +5,16 @@ namespace VoterMate.Database;
 
 public interface IDatabase
 {
-    IReadOnlyList<Household> GetHouseholds();
-    (Mobilizer, Location)? GetMobilizer(string voterID);
-    IReadOnlyCollection<Voter> GetPriorityVoters(Location location, Mobilizer mobilizer);
-    Task LoadShownFriends(FileResult file);
+    Task<IReadOnlyList<Household>> GetHouseholdsAsync();
+    Task<(Mobilizer, Location)?> GetMobilizerAsync(string voterID);
+    Task<IReadOnlyCollection<Voter>> GetPriorityVotersAsync(Location location, Mobilizer mobilizer);
+    Task LoadShownFriendsAsync(FileResult file);
     void LoadTurfList(string path);
-    void SaveShownFriends();
-    IReadOnlyCollection<string> GetNameParts();
-    IReadOnlyCollection<Voter> GetVoters(string namePart);
-    IReadOnlyList<Voter> GetVotersByName(string name);
-    IReadOnlyList<Voter> GetVotersByBirthdate(DateTime date);
+    Task SaveShownFriendsAsync();
+    Task<IReadOnlyCollection<string>> GetNamePartsAsync();
+    Task<IReadOnlyCollection<Voter>> GetVotersAsync(string namePart);
+    Task<IReadOnlyList<Voter>> GetVotersByNameAsync(string name);
+    Task<IReadOnlyList<Voter>> GetVotersByBirthdateAsync(DateTime date);
 }
 
 public record Household(string Address, Location Location, List<Mobilizer> Mobilizers)

VoterMate/Database/TsvDatabase.cs
@@ -5,51 +5,98 @@ namespace VoterMate.Database;
 internal class TsvDatabase : IDatabase
 {
     // voterID -> List<voterID>
-    private readonly Dictionary<string, HashSet<string>> _housemates = [];
-    // address -> (Location, List<voterID>)
-    private readonly List<Household> _households = [];
+    private readonly Task<Dictionary<string, HashSet<string>>> _housemates;
+    private Task<List<Household>>? _households;
     // voterID -> Voter
-    private readonly Dictionary<string, Voter> _voters = [];
-    private readonly Dictionary<string, List<Voter>> _voterAddresses = [];
-    private readonly HashSet<Voter> _priorityVoters = [];
+    private readonly Task<Dictionary<string, Voter>> _voters;
+    private readonly Task<Dictionary<string, List<Voter>>> _voterAddresses;
+    private readonly Task<HashSet<Voter>> _priorityVoters;
     private readonly Dictionary<string, HashSet<string>> _friendsShown = [];
-    private readonly Dictionary<string, List<Voter>> _voterNames = new(StringComparer.InvariantCultureIgnoreCase);
-    private readonly Dictionary<string, List<Voter>> _voterNameParts = new(StringComparer.InvariantCultureIgnoreCase);
-    private readonly Dictionary<DateTime, List<Voter>> _voterBirth = new();
+    private readonly Task<Dictionary<string, List<Voter>>> _voterNames;
+    private readonly Task<Dictionary<string, List<Voter>>> _voterNameParts;
+    private readonly Task<Dictionary<DateTime, List<Voter>>> _voterBirth;
 
     public TsvDatabase()
     {
-        using (StreamReader sr = new(typeof(TsvDatabase).Assembly.GetManifestResourceStream("VoterMate.Database.housemates.tsv")!))
+        _housemates = Task.Run(() =>
+        {
+            Dictionary<string, HashSet<string>> housemates = [];
+            using StreamReader sr = new(typeof(TsvDatabase).Assembly.GetManifestResourceStream("VoterMate.Database.housemates.tsv")!);
             while (sr.ReadLine() is string line)
             {
                 var parts = line.Split('\t');
-                _housemates[parts[0]] = [.. parts[1..]];
+                housemates[parts[0]] = [.. parts[1..]];
             }
+            return housemates;
+        });
 
-        using (StreamReader sr = new(typeof(TsvDatabase).Assembly.GetManifestResourceStream("VoterMate.Database.voters.tsv")!))
+        _voters = Task.Run(() =>
+        {
+            Dictionary<string, Voter> voters = [];
+            using StreamReader sr = new(typeof(TsvDatabase).Assembly.GetManifestResourceStream("VoterMate.Database.voters.tsv")!);
             while (Voter.LoadFrom(sr) is Voter voter)
+                voters[voter.ID] = voter;
+            return voters;
+        });
+
+        _voterAddresses = Task.Run(async () =>
+        {
+            Dictionary<string, List<Voter>> voterAddresses = [];
+            foreach (var voter in (await _voters).Values)
             {
-                _voters[voter.ID] = voter;
-                if (!_voterAddresses.TryGetValue(voter.Address, out var household))
-                    _voterAddresses[voter.Address] = household = [];
+                if (!voterAddresses.TryGetValue(voter.Address, out var household))
+                    voterAddresses[voter.Address] = household = [];
                 household.Add(voter);
-                string name = voter.Name.Replace("  ", " ");
-                if (!_voterNames.TryGetValue(name, out var names)) _voterNames[name] = names = [];
-                names.Add(voter);
-                if (!_voterBirth.TryGetValue(voter.BirthDate, out names)) _voterBirth[voter.BirthDate] = names = [];
-                names.Add(voter);
             }
+            return voterAddresses;
+        });
 
-        using (StreamReader sr = new(typeof(TsvDatabase).Assembly.GetManifestResourceStream("VoterMate.Database.priorityVoters.tsv")!))
+        _voterNames = Task.Run(async () =>
+        {
+            Dictionary<string, List<Voter>> voterNames = new(StringComparer.InvariantCultureIgnoreCase);
+            foreach (var voter in (await _voters).Values)
+            {
+                string name = voter.Name.Replace("  ", " ");
+                if (!voterNames.TryGetValue(name, out var voters)) voterNames[name] = voters = [];
+                voters.Add(voter);
+            }
+            return voterNames;
+        });
+
+        _voterBirth = Task.Run(async () =>
+        {
+            Dictionary<DateTime, List<Voter>> voterBirth = [];
+            foreach (var voter in (await _voters).Values)
+            {
+                if (!voterBirth.TryGetValue(voter.BirthDate, out var voters)) voterBirth[voter.BirthDate] = voters = [];
+                voters.Add(voter);
+            }
+            return voterBirth;
+        });
+
+        _priorityVoters = Task.Run(async () =>
+        {
+            var voters = await _voters;
+            HashSet<Voter> priorityVoters = [];
+            using StreamReader sr = new(typeof(TsvDatabase).Assembly.GetManifestResourceStream("VoterMate.Database.priorityVoters.tsv")!);
             while (sr.ReadLine() is string line)
-                _priorityVoters.Add(_voters[line]);
+                priorityVoters.Add(voters[line]);
+            return priorityVoters;
+        });
 
-        using (StreamReader sr = new(typeof(TsvDatabase).Assembly.GetManifestResourceStream("VoterMate.Database.lookupDb.tsv")!))
+
+        _voterNameParts = Task.Run(async () =>
+        {
+            var voters = await _voters;
+            Dictionary<string, List<Voter>> voterNameParts = new(StringComparer.InvariantCultureIgnoreCase);
+            using StreamReader sr = new(typeof(TsvDatabase).Assembly.GetManifestResourceStream("VoterMate.Database.lookupDb.tsv")!);
             while (sr.ReadLine() is string line)
             {
                 var parts = line.Split('\t');
-                _voterNameParts[parts[0]] = [.. parts[1..].Select(v => _voters[v])];
+                voterNameParts[parts[0]] = [.. parts[1..].Select(v => voters[v])];
             }
+            return voterNameParts;
+        });
 
         File.OpenWrite(Path.Combine(FileSystem.Current.AppDataDirectory, "friendsShown.csv")).Close();
         using (StreamReader sr = new(Path.Combine(FileSystem.Current.AppDataDirectory, "friendsShown.csv")))
@@ -58,57 +105,62 @@ internal class TsvDatabase : IDatabase
 
     public void LoadTurfList(string path)
     {
-        _households.Clear();
-        HashSet<string> addresses = [];
-        using StreamReader sr = new(path);
-        while (Household.LoadFrom(sr, _voterAddresses) is Household household)
-            if (addresses.Add(household.Address))
-                _households.Add(household);
+        _households = Task.Run(async () =>
+        {
+            List<Household> households = [];
+            HashSet<string> addresses = [];
+            using StreamReader sr = new(path);
+            while (Household.LoadFrom(sr, await _voterAddresses) is Household household)
+                if (addresses.Add(household.Address))
+                    households.Add(household);
+            return households;
+        });
     }
 
-    public IReadOnlyList<Household> GetHouseholds() => _households.AsReadOnly();
+    public async Task<IReadOnlyList<Household>> GetHouseholdsAsync() => (await (_households ?? Task.FromResult(new List<Household>()))).AsReadOnly();
 
-    public IReadOnlyCollection<Voter> GetPriorityVoters(Location location, Mobilizer mobilizer) => new ReadOnlyCollection(this, location, mobilizer);
+    public async Task<IReadOnlyCollection<Voter>> GetPriorityVotersAsync(Location location, Mobilizer mobilizer)
+        => new ReadOnlyCollection(await _housemates, await _priorityVoters, _friendsShown, location, mobilizer);
 
-    public (Mobilizer, Location)? GetMobilizer(string voterID)
+    public async Task<(Mobilizer, Location)?> GetMobilizerAsync(string voterID)
     {
-        if (_voters.TryGetValue(voterID, out Voter? voter))
+        if ((await _voters).TryGetValue(voterID, out Voter? voter))
         {
             return (new Mobilizer(voterID, voter.Name, voter.BirthDate), voter.Location);
         }
         return null;
     }
 
-    public void SaveShownFriends()
+    public Task SaveShownFriendsAsync()
     {
-        File.WriteAllLines(Path.Combine(FileSystem.Current.AppDataDirectory, "friendsShown.csv"),
+        return File.WriteAllLinesAsync(Path.Combine(FileSystem.Current.AppDataDirectory, "friendsShown.csv"),
             [.. _friendsShown.SelectMany(kvp => kvp.Value.Select(val => kvp.Key + ',' + val))]);
     }
 
-    public async Task LoadShownFriends(FileResult file)
+    public async Task LoadShownFriendsAsync(FileResult file)
     {
         using Stream stream = await file.OpenReadAsync();
         using StreamReader sr = new(stream);
         LoadShownFriends(sr);
     }
 
-    public IReadOnlyList<Voter> GetVotersByName(string name)
+    public async Task<IReadOnlyList<Voter>> GetVotersByNameAsync(string name)
     {
-        _ = _voterNames.TryGetValue(name, out var voters);
+        _ = (await _voterNames).TryGetValue(name, out var voters);
         return voters ?? [];
     }
 
-    public IReadOnlyCollection<string> GetNameParts() => _voterNameParts.Keys;
+    public async Task<IReadOnlyCollection<string>> GetNamePartsAsync() => (await _voterNameParts).Keys;
 
-    public IReadOnlyCollection<Voter> GetVoters(string namePart)
+    public async Task<IReadOnlyCollection<Voter>> GetVotersAsync(string namePart)
     {
-        _ = _voterNameParts.TryGetValue(namePart, out var voters);
+        _ = (await _voterNameParts).TryGetValue(namePart, out var voters);
         return voters ?? [];
     }
 
-    public IReadOnlyList<Voter> GetVotersByBirthdate(DateTime date)
+    public async Task<IReadOnlyList<Voter>> GetVotersByBirthdateAsync(DateTime date)
     {
-        _ = _voterBirth.TryGetValue(date, out var voters);
+        _ = (await _voterBirth).TryGetValue(date, out var voters);
         return voters ?? [];
     }
 
@@ -136,25 +188,26 @@ internal class TsvDatabase : IDatabase
 
         public int Count => _voters.Count;
 
-        public ReadOnlyCollection(TsvDatabase tsvDatabase, Location location, Mobilizer mobilizer)
+        public ReadOnlyCollection(Dictionary<string, HashSet<string>> housemates, HashSet<Voter> priorityVoters, Dictionary<string, HashSet<string>> friendsShown, Location location, Mobilizer mobilizer)
         {
             _location = location;
             _mobilizer = mobilizer;
-            if (mobilizer.ID == null || !tsvDatabase._housemates.TryGetValue(mobilizer.ID, out _housemates!))
+            if (mobilizer.ID == null || !housemates.TryGetValue(mobilizer.ID, out _housemates!))
                 _housemates = [];
 
             if (mobilizer.ID != null)
             {
-                if (!tsvDatabase._friendsShown.TryGetValue(mobilizer.ID, out _viewedFriends!))
-                    _viewedFriends = tsvDatabase._friendsShown[mobilizer.ID] = [];
+                if (!friendsShown.TryGetValue(mobilizer.ID, out _viewedFriends!))
+                    _viewedFriends = friendsShown[mobilizer.ID] = [];
             }
             else
                 _viewedFriends = [];
 
             _initialViewedFriends = [.. _viewedFriends];
 
-            _voters = tsvDatabase._priorityVoters.Where(v => v.ID != mobilizer.ID).ToList();
+            _voters = priorityVoters.Where(v => v.ID != mobilizer.ID).ToList();
         }
+
 
         public IEnumerator<Voter> GetEnumerator()
         {

VoterMate/LookupPage.xaml
@@ -8,7 +8,7 @@
              NavigatedTo="ContentPage_NavigatedTo">
     <VerticalStackLayout>
         <Label Text="Look up by partial name/address:" Margin="3"/>
-        <sf:SfAutocomplete x:Name="acVoterName" Margin="20,3,3,3" SelectionChanged="acVoterName_SelectionChanged" Completed="cboVoterName_SelectionChanged" Placeholder="Enter name, age, and/or address" SelectionMode="Multiple" MinimumPrefixCharacters="2" TextSearchMode="Contains"/>
+        <sf:SfAutocomplete x:Name="acVoterName" Margin="20,3,3,3" SelectionChanged="acVoterName_SelectionChanged" Completed="cboVoterName_SelectionChanged" Placeholder="Loading data; please wait." SelectionMode="Multiple" MinimumPrefixCharacters="2" TextSearchMode="Contains" IsEnabled="False"/>
         <Label x:Name="lblWarning" Text="No voters match all filters. Showing voters that match all but one filter." TextColor="Red" IsVisible="False" Margin="20,3,3,3"/>
         <Label Text="Look up by birthdate:" Margin="3"/>
         <DatePicker MinimumDate="1927/01/01" MaximumDate="2006/11/05" Format="MMM dd, yyyy" Margin="20,3,3,3" DateSelected="DatePicker_DateSelected" />

VoterMate/MainPage.xaml.cs
@@ -196,7 +196,7 @@ public partial class MainPage : ContentPage, INotifyPropertyChanged
                 {
                     LogEvent("Opening mobilizer page (selected)", mobilizer.ID, _location);
                     App.DoorsKnocked.AddValue(new(Canvasser, household.Address, mobilizer.ID!));
-                    await Navigation.PushAsync(new MobilizerPage(household.Location, mobilizer, this));
+                    await Navigation.PushAsync(new MobilizerPage(household.Location, mobilizer, await App.Database.GetPriorityVotersAsync(location, mobilizer), this));
                     DoorKnocked((Button)expander.Header);
                 }
                 async void AddNote(object? sender, EventArgs e)
@@ -257,7 +257,7 @@ public partial class MainPage : ContentPage, INotifyPropertyChanged
         double squareFilterLongitude = locationFilterRange / 1.609 / MilesPerDegree;
         double squareFilterLatitude = locationFilterRange / 1.609 / MilesPerDegree / Math.Cos(location.Longitude);
 
-        var households = App.Database.GetHouseholds();
+        var households = await App.Database.GetHouseholdsAsync();
 
         if (households.Count == 0)
         {
@@ -385,15 +385,16 @@ public partial class MainPage : ContentPage, INotifyPropertyChanged
         Geolocation.StartListeningForegroundAsync(new GeolocationListeningRequest(GeolocationAccuracy.Best));
     }
 
-    private void NotListed_Clicked(object? sender, EventArgs e)
+    private async void NotListed_Clicked(object? sender, EventArgs e)
     {
         if (_location != null)
         {
             LogEvent("Opening mobilizer page (not listed)", null, _location);
-            Navigation.PushAsync(new MobilizerPage(_location, null, this));
+            var mobilizer = new Mobilizer(null, string.Empty, null);
+            await Navigation.PushAsync(new MobilizerPage(_location, mobilizer, await App.Database.GetPriorityVotersAsync(_location, mobilizer), this));
         }
         else
-            DisplayAlert("Unknown location", $"Friend lists cannot be displayed without a voter ID or location information.", "OK");
+            await DisplayAlert("Unknown location", $"Friend lists cannot be displayed without a voter ID or location information.", "OK");
     }
 
     private async void Copy_Clicked(object? sender, EventArgs e)

VoterMate/MobilizerPage.xaml.cs
@@ -15,13 +15,13 @@ public partial class MobilizerPage : ContentPage
 
     public Mobilizer Mobilizer => _mobilizer;
 
-    public MobilizerPage(Location location, Mobilizer? mobilizer, MainPage mainPage)
+    public MobilizerPage(Location location, Mobilizer mobilizer, IReadOnlyCollection<Voter> voters, MainPage mainPage)
     {
         InitializeComponent();
 
         _location = location;
 
-        if (mobilizer != null)
+        if (mobilizer.ID != null)
         {
             nameRow.Height = new GridLength(0);
             Title = mobilizer.Name;
@@ -29,9 +29,9 @@ public partial class MobilizerPage : ContentPage
         else
             btnEdit.IconImageSource = null;
 
-        _mobilizer = mobilizer ?? new Mobilizer(null, string.Empty, null);
+        _mobilizer = mobilizer;
         _mainPage = mainPage;
-        _voters = App.Database.GetPriorityVoters(location, _mobilizer);
+        _voters = voters;
         for (int i = 0; i < 101; i++)
             dgVoters.RowDefinitions.Add(new(GridLength.Auto));
 
@@ -125,7 +125,7 @@ public partial class MobilizerPage : ContentPage
             mobilizerContacted = true;
         }
 
-        App.Database.SaveShownFriends();
+        await App.Database.SaveShownFriendsAsync();
 
         if (mobilizerContacted)
         {
@@ -142,7 +142,7 @@ public partial class MobilizerPage : ContentPage
         }
     }
 
-    internal static Task LoadShownFriendsData(FileResult file) => App.Database.LoadShownFriends(file);
+    internal static Task LoadShownFriendsData(FileResult file) => App.Database.LoadShownFriendsAsync(file);
 
     private async void Edit_Clicked(object sender, EventArgs e)
     {

commit c01dccb88ac171364a4b837f5d87e353c949c8cd

Fix a crash when the voter id is not found. (fixes #50)
Regression from ced2347c

VoterMate/LookupPage.xaml.cs
@@ -38,14 +38,14 @@ public partial class LookupPage : ContentPage
 
     private async void Lookup_Clicked(object sender, EventArgs e)
     {
-        var info = App.Database.GetMobilizerAsync("OH" + txtVoterID.Text);
+        var info = await App.Database.GetMobilizerAsync("OH" + txtVoterID.Text);
         if (info == null)
         {
             await DisplayAlert("Not Found", $"The voter with ID OH{txtVoterID.Text} could not be found.", "OK");
             return;
         }
 
-        var (mobilizer, location) = (await info).Value;
+        var (mobilizer, location) = info.Value;
         _mainPage.LogEvent("Opening mobilizer page (ID lookup)", mobilizer.ID, _mainPage.Location);
         await txtVoterID.HideSoftInputAsync(new CancellationTokenSource().Token);
         await Navigation.PushAsync(new MobilizerPage(location, mobilizer, await App.Database.GetPriorityVotersAsync(location, mobilizer), _mainPage));